78d508fa96a653e4d4dbe358525347b4c3ff7c2b
Emil Mirzayev <mirzayev_emil@yahoo.com>
2019-07-29 13:04:57 +0200

    Update requirements.sh from editor

--- requirements.sh
+++ requirements.sh
@@ -1,7 +1,6 @@
 # If bash command fails, build should error out
 set -e
-pip install matplotlib
-pip install seaborn
+
 
 # Install specific package versions with pip
 # Check the latest version using https://pypi.org/search
@@ -12,7 +11,8 @@ pip install seaborn
 # pip3 install numpy==1.15.0
 # pip3 install seaborn==0.9.0
 # pip3 install bokeh==0.13.0
-
+pip3 install matplotlib
+pip3 install seaborn
 
 # Include datasets in the image
 
